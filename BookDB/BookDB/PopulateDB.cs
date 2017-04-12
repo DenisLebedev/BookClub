7fb323d216894c0159e4f6bec08f5ec231c29734
Cookie <1532444@2F16-32>
2017-04-12 15:31:17 -0400

    Book/author settings

--- BookDB/BookDB/PopulateDB.cs
+++ BookDB/BookDB/PopulateDB.cs
@@ -23,26 +23,70 @@ namespace BookDB
         /// </summary>
         public PopulateDB()
         {
-            bookEl = XElement.Load("books.xml");
-            ratingEl = XElement.Load("ratings.xml");
+
+            bookEl = XElement.Load("../../books.xml");
+            ratingEl = XElement.Load("../../ratings.xml");
 
         }
 
 
         public void AddDataInDB()
         {
-            var test = from item in bookEl.Descendants("book")
-                       select new {};
+
+            IEnumerable<Book> books = ListOfBooks();
+            Console.WriteLine(books);
+            
         }
 
+        public IEnumerable<Author> ListOfAuthor()
+        {
+            IEnumerable<Author> list =
+                 from item in bookEl.Descendants("book")
+                 select CreateAuthorObject(item);
 
+            return list;
+        }
+
+        public Author CreateAuthorObject(XElement item)
+        {
+            Console.WriteLine("Creating author");
+
+            Author obj = new Author();
+            obj.FirstName = item.Element("author").Attribute("firstName")?.Value;
+            obj.LastName = item.Element("author").Attribute("lastName")?.Value;
+
+            return obj;
+        }
+
+        public IEnumerable<Book> ListOfBooks()
+        {
+            IEnumerable<Book> list =
+                from item in bookEl.Descendants("book")
+                select CreateBookObject(item);
+
+            return list;
+        }
+
+        private Book CreateBookObject(XElement item)
+        {
+            Console.WriteLine("Creating book");
+
+            Book obj = new Book();
+            obj.Title = item.Element("title")?.Value;
+            obj.Description = item.Element("description")?.Value;
+            obj.BookId = Int32.Parse(item.Attribute("id").Value);
+            return obj;
+        }
 
         static void Main(string[] args)
         {
 
             //books.xml  ratings.xml
-
+            
             PopulateDB app = new PopulateDB();
+            app.AddDataInDB();
+
+            Console.Read();
 
         }
 
