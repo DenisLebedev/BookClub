f2320d0883098241c19498f7c2fd736ee7e58afb
Cookie <1532444@2F16-32>
2017-04-28 15:19:56 -0400

    Input data Done

--- BookDB/BookDB/PopulateDB.cs
+++ BookDB/BookDB/PopulateDB.cs
@@ -34,27 +34,10 @@ namespace BookDB
            
             IEnumerable<Author> authors = ListOfAuthor(bookEl);
             IEnumerable<Book> books = ListOfBook(bookEl, authors);
-            IEnumerable<User> users = ListOfUser(ratingEl);
-            IEnumerable<Review> reviews = ListOfReview(ratingEl, books, users);
+            IEnumerable<User> users = ListOfUser(ratingEl, books);
 
             AddAllDataInDB(authors, users);
-
-
-            /*foreach (Author i in authors)
-            {
-                Console.Write(i.FirstName);
-                foreach (Book j in i.Books)
-                    Console.Write("  " + j.Title);
-                Console.WriteLine();
-            }*/
-
-            /*foreach(User u in users)
-            {
-                foreach (Review r in u.Reviews)
-                    Console.WriteLine(u.FirstName + " " + r.Rating);
-            }*/
-            
-
+           
 
             Console.WriteLine("Done");
         }
@@ -63,62 +46,12 @@ namespace BookDB
         {
             using(BookClubDB db = new BookClubDB())
             {
-                /*foreach(Author item in authors)
-                 {
-                     try
-                     {
-                         db.Authors.Add(item);
-                         Console.WriteLine(item.AuthorId + " " + item.FirstName + " " + item.LastName);
-                         db.SaveChanges();
-                     }
-                     catch (DbEntityValidationException dbEx)
-                     {
-                         Console.WriteLine("ErrorLine: -" + item.FirstName + "-" );
-                         foreach (var validationErrors in dbEx.EntityValidationErrors)
-                         {
-                             foreach (var validationError in validationErrors.ValidationErrors)
-                             {
-                                 System.Console.WriteLine("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
-                             }
-                         }
-                     }
-                 }*/
+               
+                db.Authors.AddRange(authors);
+                db.Users.AddRange(users);
 
-                //db.Authors.AddRange(authors);
+                db.SaveChanges();
 
-                foreach (User item in users)
-                 {
-                    try
-                    {
-                        db.Users.Add(item);
-                        db.SaveChanges();
-                    }
-                    catch (DbEntityValidationException dbEx)
-                    {
-                        Console.WriteLine("ErrorLine: -" + item.FirstName + "-");
-                        foreach (var validationErrors in dbEx.EntityValidationErrors)
-                        {
-                            foreach (var validationError in validationErrors.ValidationErrors)
-                            {
-                                System.Console.WriteLine("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
-                            }
-                        }
-                    }catch(Exception e)
-                    {
-                        Console.WriteLine("General Exception: " + e.InnerException);
-                    }
-                 }
-
-                
-                //db.Users.AddRange(users);
-                /*try
-                {
-
-                    db.SaveChanges();
-                }catch(Exception e)
-                {
-                    Console.WriteLine(e.Message + e.InnerException + " <<<");
-                }*/
             }
         } 
 
@@ -143,9 +76,7 @@ namespace BookDB
             int value;
             if (Int32.TryParse(temp, out value))
                 obj.AuthorId = value;
-            /*String test = item.Element("author").Attribute("firstName")?.Value;
-            if (test == null)
-                test = "";*/
+
             obj.FirstName = GetValueOrDefault(item.Element("author").Attribute("firstName")?.Value);
             obj.LastName = item.Element("author").Attribute("lastName")?.Value;
             return obj;
@@ -173,9 +104,6 @@ namespace BookDB
                 obj.Description = item.Element("description")?.Value;
                 obj.Views = 0;
 
-                /*String test = item.Element("author").Attribute("firstName")?.Value;
-                if (test == null)
-                    test = "";*/
                 authors.Where(
                     x => x.FirstName == GetValueOrDefault(item.Element("author").Attribute("firstName")?.Value) &&
                     x.LastName == item.Element("author").Attribute("lastName")?.Value).
@@ -186,66 +114,15 @@ namespace BookDB
             return obj;
         }
 
-        private static IEnumerable<User> ListOfUser(XElement ratingEl)
+        private static IEnumerable<User> ListOfUser(XElement ratingEl, IEnumerable<Book> books)
         {
-            IEnumerable<User> list =
-                from item in ratingEl.Descendants("user")
-                select CreateUserObject(item);
+            return
+                (from item in ratingEl.Descendants("user")
+                select CreateUserObject(item, books)).ToList();
 
-            return list.ToList();
         }
 
-        private static IEnumerable<Review> ListOfReview(XElement ratingEl, 
-            IEnumerable<Book> books, IEnumerable<User> users)
-        {
-            List<Review> list = new List<Review>();
-
-            //for each user create a list of reviews
-            foreach (XElement item in ratingEl.Descendants("user"))
-            {
-                IEnumerable<Review> temp;
-                temp = CreateReviewPerUser(item.Descendants().Where(x => x.Attribute("rating") != null),
-                        users.Where(x => x.UserName == item.Attribute("userId")?.Value).First()
-                        , books);
-
-                if (temp.Count() != 0)
-                    foreach (Review i in temp)
-                        list.Add(i);
-            }
-
-            return list.ToList();
-        }
-
-        private static IEnumerable<Review> CreateReviewPerUser(IEnumerable <XElement> item, User user,
-            IEnumerable<Book> books)
-        {
-            int bookId;
-            int ratingNum;
-            List<Review> list = new List<Review>();
-
-            foreach (XElement review in item)
-            {
-                
-                if (Int32.TryParse(review.Attribute("bookId")?.Value, out bookId) &&
-                    Int32.TryParse(review.Attribute("rating")?.Value, out ratingNum))
-                {
-                    Review obj = new Review();
-
-                    //Console.WriteLine(books.Where(x => x.BookId == bookId).Count());
-
-                    Book book = books.Where(x => x.BookId == bookId).First();
-                    obj.BookId = book.BookId;
-                    book.Views += 1;
-                    obj.Rating = ratingNum;
-                    obj.UserName = user.UserName;
-                    list.Add(obj);
-                    user.Reviews.Add(obj);
-                }
-            }
-            return list;
-        }
-
-        private static User CreateUserObject(XElement item)
+        private static User CreateUserObject(XElement item, IEnumerable<Book> books)
         {
             User obj = new User();
 
@@ -253,14 +130,47 @@ namespace BookDB
             string userName = GetValueOrDefault(item.Attribute("userId").Value, "");
             obj.UserName = userName;
             obj.FirstName = userName;
-            obj.LastName = GetValueOrDefault(item.Attribute("lastName").Value, "Reader");
+            obj.LastName = GetValueOrDefault(item.Attribute("lastName")?.Value, "Reader");
             obj.Password = userName;
             obj.Country = "CAN";
             obj.Email = null;
+
+            if (obj.UserName == item.Attribute("userId")?.Value)
+                CreateReviewPerUser(item.Descendants().Where(x => x.Attribute("rating") != null),
+                obj, books);
             return obj;
         }
 
 
+        private static void CreateReviewPerUser(IEnumerable<XElement> item, User user,
+           IEnumerable<Book> books)
+        {
+            int bookId;
+            int ratingNum;
+            List<Review> list = new List<Review>();
+
+            foreach (XElement review in item)
+            {
+
+                if (Int32.TryParse(review.Attribute("bookId")?.Value, out bookId) &&
+                    Int32.TryParse(review.Attribute("rating")?.Value, out ratingNum))
+                {
+                    Review obj = new Review();
+
+                    Book book = books.Where(x => x.BookId == bookId).First();
+                    obj.Book = book;
+                    obj.BookId = book.BookId;
+                    book.Views += 1;
+                    obj.Rating = ratingNum;
+                    obj.UserName = user.UserName;
+                    obj.User = user;
+                    list.Add(obj);
+                    user.Reviews.Add(obj);
+                }
+            }
+        }
+
+       
         private static string GetValueOrDefault(string str, string def = "")
         {
             return str == null ? def : str;
@@ -269,11 +179,7 @@ namespace BookDB
 
         static void Main(string[] args)
         {
-
-            //books.xml  ratings.xml
             
-            PopulateDB app = new PopulateDB();
-            //app.AddDataInDB();
             PopulateDB.AddDataInDB();
             Console.Read();
 
