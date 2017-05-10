bd947dcc5605c2be13bd5ba527a36290fd14bf8e
Cookie <1532444@2F20-09>
2017-05-10 14:23:39 -0400

    Working on comments

--- BookDB/ASPBookClub/Controllers/HomeController.cs
+++ BookDB/ASPBookClub/Controllers/HomeController.cs
@@ -36,23 +36,33 @@ namespace ASPBookClub.Controllers
         }
 
         /// <summary>
-        /// 
+        /// Filter our database to find the 10 most viewed
+        /// books
         /// </summary>
-        /// <returns></returns>
+        /// <returns> list of books that has the highest number of views </returns>
         private List<Book> TopViewedBooks()
         {
             return (from item in db.Books
                     orderby (item.Views) descending
                     select item).Take(10).ToList();
         }
+
+        /// <summary>
+        /// Processing all the users that have read the same book as the 
+        /// given user. Then with all the users found a mathematical operation is made 
+        /// to find the best match. To continue, we take 10 books that the given user
+        /// did not read from the best matched user.
+        /// </summary>
+        /// <param name="userN"> username currently connected </param>
+        /// <returns></returns>
         private List<Book> RecommendedBooks(string userN)
         {
-
+            //represent all the reviews made by the given user
             List<Review> userRead = (from item in db.Reviews
                                      where item.UserName == userN
                                      select item).ToList();
 
-            
+            //Grouping each user with their reviews
             IEnumerable<IGrouping<string, Review>> allusersRev =
                 from bk in db.Reviews
                 where bk.UserName != userN
@@ -60,20 +70,32 @@ namespace ASPBookClub.Controllers
                 select groupedUser;
 
 
+            //Used variable to find the highest rating
             int? highRat = 0;
+
+            //Will hold the most common user (best match)
             string commonU = null;
+
+            //Iterating throught the grouped list
             foreach (var person in allusersRev)
             {
+                //Will hold ratings
                 int? temp = 0;
 
+                //Iterating to all the reviews made by person X
                 foreach (var review in person)
                 {
+                    //Grabing the rating that person X made for the book that the given user read
                     int? rating = userRead.Where(x => x.BookId ==
                             review.BookId).FirstOrDefault()?.Rating;
+
+                    //Rating is nullable
                     if (rating != null)
                         temp += rating * review.Rating;
                 }
 
+                //If temp is bigger than our highest rating we should swap
+                //and save the username of that user that has a better score
                 if (temp > highRat)
                 {
                     highRat = temp;
@@ -83,7 +105,12 @@ namespace ASPBookClub.Controllers
             }
 
            
-
+            /**
+             * Taking 10 books from the best matched user.
+             * These books should match his name and a rating
+             * higher than -1. Also, this book should not match
+             * the book that the given user already read.
+             */
             List<Book> tempList = (from item in db.Reviews
                                      where item.UserName == commonU
                                          && item.Rating >= 0
@@ -114,6 +141,16 @@ namespace ASPBookClub.Controllers
 
 
         // GET: Book/Details/5
+        /// <summary>
+        /// The get method will ensure some viewbag that will hold
+        /// the average rating, but only for a authorized user.
+        /// Also, the book that will be returned is has new rating values
+        /// for the user purpose.
+        /// The display in the view will also change depending the state of 
+        /// the user.
+        /// </summary>
+        /// <param name="id">the book id</param>
+        /// <returns> deep copy of a book </returns>
         public ActionResult DetailsBook(int? id)
         {
             
@@ -129,18 +166,32 @@ namespace ASPBookClub.Controllers
             }
             else
             {
+                /**
+                 * If the book exist that mean the user
+                 * could enter inside the website so we increment
+                 * the number of view and save the changes
+                 */ 
                 book.Views += 1;
                 db.SaveChanges();
             }
+
+            //This book object will hold the copy
             Book copy = null;
             if (User.Identity.Name != "")
             {
                 
+                //Creating a copy of the given book and change the rating for the UI
                 copy  = IncrementRatingForDisplay(book);
+
+                //Calculating the average
                 ViewBag.averageRat = AverageRating(copy);
+
+                //The method can return null so a 'default' value is necessary
                 if (ViewBag.averageRat == null)
+                    //The user will know that no review was made
                     ViewBag.averageRat = "None";
                 else
+                    //The choice about how much a person will round the rating should be give
                     ViewBag.averageRat = 
                         Math.Round(ViewBag.averageRat,2);
             }
@@ -148,10 +199,22 @@ namespace ASPBookClub.Controllers
             return View(copy);
         }
 
-
+        /// <summary>
+        /// Calculate the average rating made
+        /// by all the users from the give book.
+        /// </summary>
+        /// <param name="bk"> a book object </param>
+        /// <returns>nullable integer</returns>
         private double? AverageRating(Book bk)
         {
 
+            /**
+             * Grab all the books that has the same id
+             * which should be unique and return a  
+             * anonymous object. Then we create a list to
+             * process the result and get the average from 
+             * it using existant method.
+             */
             var avg = (from t in db.Reviews
                        where t.Book.BookId == bk.BookId
                        select new
@@ -163,8 +226,18 @@ namespace ASPBookClub.Controllers
         }
 
 
+        /// <summary>
+        /// The method will take the data from the given book
+        /// and create a deep copy of it. This way nothing will
+        /// change in our database and avoid errors. It is important
+        /// because this method will change the rating of the given book
+        /// specifically for the user view.
+        /// </summary>
+        /// <param name="book"> book object</param>
+        /// <returns>deep copy fo a book object with a new rating </returns>
         private Book IncrementRatingForDisplay(Book book)
         {
+            //Copy everything from the given book, but not the ratings
             Book copy = new Book()
             {
                 BookId = book.BookId,
@@ -173,9 +246,10 @@ namespace ASPBookClub.Controllers
                 Title = book.Title
             };
 
+            //Ratings are an ICollection
             ICollection<Review> reviewChanged = new List<Review>();
     
-
+            //Adding each review with a little change
             foreach (var t in book.Reviews)
             {
 
@@ -186,16 +260,23 @@ namespace ASPBookClub.Controllers
                     ReviewId = t.ReviewId,
                     User = t.User,
                     UserName = t.UserName,
+                    //Changing the rating
                     Rating = IncrementRating(t.Rating)
 
                 });                                
             }
-
+            
+            //Adding the new reviews
             copy.Reviews = reviewChanged;
             return copy;
         } 
 
 
+        /// <summary>
+        /// Increment the rating for the UI.
+        /// </summary>
+        /// <param name="rating">integer that represent the current rating</param>
+        /// <returns></returns>
         private int? IncrementRating(int? rating)
         {
             if (rating == -5)
@@ -206,20 +287,27 @@ namespace ASPBookClub.Controllers
                 rating = 3;
             else if (rating == 3)
                 rating = 4;
-            
+            else
+                //Default value
+                rating = null;
 
             return rating;
         }
 
 
         // GET: Book/Create
+        /// <summary>
+        /// The get method will ensure that the necessary
+        /// viewbags are made to display the dropdown and used to 
+        /// redirect to the right view.
+        /// </summary>
+        /// <returns></returns>
         [Authorize]
         public ActionResult CreateBook()
         {
 
+            //Creation of viewbags
             CreateListForDropDown();
-
-
 
             return View();
         }
@@ -227,6 +315,15 @@ namespace ASPBookClub.Controllers
         // POST: Book/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// The post method will ensure that the given data
+        /// is right and will adda new book to the database.
+        /// The method is only available for an autorized user.
+        /// </summary>
+        /// <param name="book">the new book object</param>
+        /// <param name="searchListOne">the choosed author in the first drop down</param>
+        /// <param name="searchListTwo">the choosed author in the second drop down</param>
+        /// <returns></returns>
         [HttpPost]
         [ValidateAntiForgeryToken]
         [Authorize]
@@ -237,41 +334,63 @@ namespace ASPBookClub.Controllers
             if(book == null)
                 return HttpNotFound();
 
+            //The first one must be selected
             if (searchListOne == "default")
             {
+                //Recreating the dropdowns 
+                CreateListForDropDown();
+
+                //Meaningful message
                 ModelState.AddModelError("", "Error: you must select an author from the first dropdown");
                 return View(book);
             }
 
             if (ModelState.IsValid)
-            {
+            {   
+                //The way that value of the viewbags are setted allow to use the split method
                 string[] authLnFn = searchListOne.Split(' ');
+
+                //Index 0 :Last name, Index 1: First name, the method return the current author
                 Author auth1 = GetAuthorPerName(authLnFn[1], authLnFn[0]);
 
+                //The user chooed 2 authors
                 if (searchListTwo != "default")
                 {
+                    //Perform the same action on the second selected element
                     authLnFn = searchListTwo.Split(' ');
+
                     Author auth2 = GetAuthorPerName(authLnFn[1], authLnFn[0]);
 
+                    //If the two objects are the same the user should restart
                     if (auth1 == auth2)
                     {
+                        //Recreate the dropdowns
                         CreateListForDropDown();
+
+                        //Meaningful message
                         ModelState.AddModelError("", "Error: you selected the same author twice");
                         return View(book);
                     }
 
+                    //If they are not the same object we can easily add them
                     book.Authors.Add(auth1);
                     book.Authors.Add(auth2);
+
                 } else
                 {
+                    //Else we only add one book
                     book.Authors.Add(auth1);
                 }
 
 
 
-
+                //Add the book
                 db.Books.Add(book);
+
+                //Save changes
                 db.SaveChanges();
+
+                //Redirect to the main page
                 return RedirectToAction("Index");
             }
 
@@ -279,19 +398,33 @@ namespace ASPBookClub.Controllers
         }
 
 
+        /// <summary>
+        /// Creates 2 viewbag that will be used to display
+        /// two dropdown in the CreateBook method. 
+        /// The viewbags will hold all authors so the user
+        /// can select one
+        /// </summary>
         private void CreateListForDropDown()
         {
             List<SelectListItem> items = new List<SelectListItem>();
+
+            //First value that the user will see or choose if they do not want to link a author
             items.Add(new SelectListItem { Text = "None", Value = "default", Selected = true });
+
+            //Iterating through all authors in our database
             foreach (Author t in db.Authors)
             {
+                //For each author we create a new item
                 items.Add(new SelectListItem
-                {
+                {   
+                    //What the user will see
                     Text = (t.LastName + ", " + t.FirstName),
+                    //What the data is actually
                     Value = (t.LastName + " " + t.FirstName)
                 });
             }
 
+            //Adding to the viewBags
             ViewBag.SearchListOne = items;
             ViewBag.SearchListTwo = items;
         }
@@ -304,7 +437,17 @@ namespace ASPBookClub.Controllers
                     select t).FirstOrDefault();
         }
 
+
         // GET: Author/Details/5
+        /// <summary>
+        /// The get request will print the details of an author.
+        /// An author has written many books and so we will
+        /// create a new author object and give him all the books
+        /// that author written sorted by the number of views.
+        /// The method is only available for an autorized user.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [Authorize]
         public ActionResult DetailsAuthor(int? id)
         {
@@ -318,6 +461,10 @@ namespace ASPBookClub.Controllers
                 return HttpNotFound();
             }
 
+            /**
+             * The author will contain a list of books sorted
+             * that why creating a new object is necessary.  
+             */
             Author temp = new Author()
             {
                 Books = author.Books.OrderByDescending(x => x.Views).ToList(),
